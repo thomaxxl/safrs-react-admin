7547d70f60b10878754b37a0f808f44bce260a5d
Thomas Pollet <thomas.pollet@gmail.com>
2023-10-12 08:36:59 +0200

    sso textfield

--- src/pages/SSOLogin.js
+++ src/pages/SSOLogin.js
@@ -2,50 +2,35 @@ import { useState} from "react";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { useConf } from "../Config";
-import { Input } from "@mui/material";
-
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import { TextField } from "@mui/material";
 
 const RedirBtn = () => {
     const conf = useConf();
-    const classes = useStyles();  
     const [redir, setRedir] = useState(false);
+    const [jwtValue, setJwtValue] = useState('');
+
+    const handleTextFieldChange = (event) => {
+        setJwtValue(event.target.value);
+    };
+
+    const saveJwt = () => {
+        localStorage.setItem('auth_token', jwtValue)
+    }
+    
 
     if(redir){
         window.location.href = conf.authentication.redirect;
     }
 
-    return <div className={classes.paper}>
+    return <div>
         
-        <Button onClick={()=>setRedir(1)} color="primary" variant="outlined" >Click to redirect to {conf.authentication.sso}</Button>
-        
+        <Button onClick={()=>setRedir(1)} color="primary" variant="outlined" >Click to redirect to {conf.authentication.sso} (conf.authentication.sso)</Button>
+        <TextField label="JWT" variant="outlined" value={jwtValue} onChange={handleTextFieldChange}></TextField>
+        <br/>
+        <Button onClick={()=>saveJwt()} color="primary" variant="outlined" placeholder="ey..">Save JWT to localStorage</Button>
         </div>
 }
 
